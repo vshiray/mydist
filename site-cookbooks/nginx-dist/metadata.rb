f587621f686b89535c5828f918cd5f9ad6f57098
root <root@vagrant.vm>
2015-04-14 08:13:55 +0000

    Update

---
+++ site-cookbooks/nginx-dist/metadata.rb
@@ -0,0 +1,9 @@
+name             'nginx-dist'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures nginx-dist'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends          'apt'
---
+++ site-cookbooks/nginx-dist/recipes/default.rb
@@ -0,0 +1,19 @@
+#
+# Cookbook Name:: nginx-dist
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe 'apt'
+
+apt_repository 'nginx' do
+  uri           'http://nginx.org/packages/ubuntu/'
+  distribution  node['lsb']['codename']
+  components    ['nginx']
+  key           'http://nginx.org/keys/nginx_signing.key'
+end
+
+package 'nginx'
