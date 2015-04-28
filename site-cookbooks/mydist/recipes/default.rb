128b326835a7f2b2932b90f7a2fc3a8c4661be6f
root <root@vagrant.vm>
2015-04-28 05:22:53 +0000

    Update

--- site-cookbooks/mydist/recipes/default.rb
+++ site-cookbooks/mydist/recipes/default.rb
@@ -40,7 +40,11 @@ else
 end
 
 if myips.include?(node['mydist']['social']['host']) || myips.include?(node['mydist']['conceptus']['host'])
-    #include_recipe 'java'
+    node.default['java']['install_flavor'] = 'oracle'
+    node.default['java']['jdk_version'] = '8'
+    node.default['java']['oracle']['accept_license_agreement'] = 'true'
+    node.default['java']['oracle']['accept_oracle_download_terms'] = 'true'
+    include_recipe 'java'
 end
 
 if myips.include?(node['mydist']['social']['host'])
